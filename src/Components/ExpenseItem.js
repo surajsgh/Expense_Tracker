475ab70a346b5241a74b6e0783dda4b266aa485e
Suraj Singh <surajsingh220698@outlook.com>
2021-04-25 21:51:39 +0530

    Show the dynamic content to the ExpenseItem component.

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date().toISOString().slice(0, 10);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.7;
+
   return (
     <div className="expense-item">
-      <div>April 25, 2021</div>
+      <div>{expenseDate}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.7</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
