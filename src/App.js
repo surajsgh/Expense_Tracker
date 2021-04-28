6f47bd1796d9f4dbe3fc17eab1202a757928d507
Suraj Singh <surajsingh220698@outlook.com>
2021-04-28 20:28:14 +0530

    Render the list items dynamically.

--- src/App.js
+++ src/App.js
@@ -5,32 +5,37 @@ import NewExpense from './Components/NewExpenses/NewExpense';
 import ExpenseFilter from './Components/Expenses/ExpenseFilter';
 import './Components/Expenses/Expenses.css';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const [selectYear, setSelectYear] = useState('2021');
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
 
   const addExpenseHandler = savedExpenseData => {
     console.log('Save data!');
+    setExpenses(prevExpenses => {
+      return [savedExpenseData, ...prevExpenses];
+    });
     console.log(savedExpenseData);
   };
 
